ea6d645fd65fe0d47b20c83a3cab9a559adfd677
Eugene Pashkeivch <eugene.pashkevich@1pt.com>
2023-05-24 00:24:22 +0300

    init canvas

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('hello!');
+import { Application } from 'pixi.js';
+
+const app = new Application<HTMLCanvasElement>({
+  width: 1400,
+  height: 800,
+  backgroundColor: 'blue',
+});
+
+document.body.appendChild(app.view);
+
+app.view.width = 1400;
+app.view.height = 800;
